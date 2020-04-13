c053ce49505360e57ca23e6d2c4b52fefc62b180
Raheel Y <ray37@cornell.edu>
2020-04-13 19:08:32 -0400

    nav mobile ready ish

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -13,6 +13,14 @@ export class AppComponent {
   scrollTop(event: any) {
     document.body.scrollTop = 0;
     document.body.scrollLeft = 0;
+
+    let x = document.querySelector("nav.nav");
+    let menu = document.querySelector(".menu i");
+    if (x.className === "nav responsive") {
+      x.className = "nav";
+      menu.id = "menu";
+      menu.textContent = "menu";
+    }
   }
 }
 
